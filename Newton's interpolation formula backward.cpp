57cdf149e6fc1f3505015f8ac49b9bec29affc95
Fariya Ahmed punom <fariyaahmed874@gmail.com>
2025-01-31 23:20:05 +0800

    Update Newton's interpolation formula backward.cpp

--- Newton's interpolation formula backward.cpp
+++ Newton's interpolation formula backward.cpp
@@ -1,54 +1,82 @@
-// CPP Program to interpolate using
-// newton backward interpolation
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-float u_cal(float u, int n)
+int factorial(int n)
 {
-	float temp = u;
-	for (int i = 1; i < n; i++)
-		temp = temp * (u + i);
-	return temp;
-}
-int fact(int n)
-{
-	int f = 1;
-	for (int i = 2; i <= n; i++)
-		f *= i;
-	return f;
+    if(n==0 || n==1)
+    {
+        return 1;
+    }
+    return n * factorial(n-1);
 }
 
-int main()
+double interpolation(vector<vector<double>> &a,double p,int n)
 {
-	int n = 5;
-	float x[] = { 1891, 1901, 1911,
-				1921, 1931 };
-	float y[n][n];
-	y[0][0] = 46;
-	y[1][0] = 66;
-	y[2][0] = 81;
-	y[3][0] = 93;
-	y[4][0] = 101;
-	for (int i = 1; i < n; i++) {
-		for (int j = n - 1; j >= i; j--)
-			y[j][i] = y[j][i - 1] - y[j - 1][i - 1];
-	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <= i; j++)
-			cout << setw(4) << y[i][j]
-				<< "\t";
-		cout << endl;
-	}
-	float value = 1925;
-	float sum = y[n - 1][0];
-	float u = (value - x[n - 1]) / (x[1] - x[0]);
-	for (int i = 1; i < n; i++) {
-		sum = sum + (u_cal(u, i) * y[n - 1][i]) /
-									fact(i);
-	}
+   double result = a[0][n-1];
+   double term = 1.0;
+   double term2 = 0.0;
+   int j = n - 2;
+   for(int i = 1; i < n; i++)
+   {
+    term*=(p+(i-1));
+    term2 = term / factorial(i);
+    result+= term2*a[i][j];
+    j--;
+   }
 
-	cout << "\n Value at " << value << " is "
-		<< sum << endl;
-	return 0;
+   return result;
 }
+
+int main ()
+{
+    cout << "Enter The Number of Elemnts - ";
+    int n;
+    cin >> n;
+    cout << endl;
+
+    cout << "Enter The Last Number of X - ";
+    double x;
+    cin >> x;
+    cout << endl;
+
+    cout << "Enter The Finding Number of X - ";
+    double x1;
+    cin >> x1;
+    cout << endl;
+
+    cout << "Enter The Difference - ";
+    double diff;
+    cin >> diff;
+    cout << endl;
+
+    vector<vector<double>> a(n,vector<double>(n));
+
+    for(int i  = 0; i < 1; i++)
+    {
+        for(int j = 0; j <n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    int k= n-1;
+
+
+    for(int i = 1; i < n; i++)
+    {
+        cout << "Del " << i << " " << endl;
+        for(int j = 0; j < k; j++)
+        {
+            a[i][j] = a[i-1][j+1] - a[i-1][j];
+            cout<< fixed << setprecision(6) << a[i][j] << endl;
+        }
+        k = k - 1;
+    }
+
+    double p = (x1 - x) / diff;
+
+    cout << interpolation(a,p,n) << endl;
+
+}
+
+
 
